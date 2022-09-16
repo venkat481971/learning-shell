65dd0af6685deeaa2bc3c9dafa639e7cb7c29b8f
venkat <venkymec1922@gmail.com>
2022-09-16 18:20:58 +0530

    functions

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,14 +1,23 @@
 ## Declaration way1
 
 function SAMPLE() {
+  echo a from main program = $a
+  a=100
+  echo a in function after override in function - $a
   echo SAMPLE FUNCTION
+  b=10
+  echo First Argument to Script = $1
 }
-
 
 ## Declaration way2
 SAMPLE1() {
   echo SAMPLE FUNCTION
 }
 
-SAMPLE
-SAMPLE1
+## Main program
+a=100
+SAMPLE angel
+#SAMPLE1
+echo b from function = $b
+b=200
+echo b from function after override in Main program - $b
