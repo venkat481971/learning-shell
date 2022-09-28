502d8235a006757cb22e62401bd0545ac6d262cc
venkat <venkymec1922@gmail.com>
2022-09-28 10:15:27 +0530

    ansible

--- 01-shebang.sh
+++ 01-shebang.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
 # #! is a she-bang
 # completely optional
 # adding that will have an understanding of script
 
 ls
+
 # shell scripting is nothing but adding all your manual commands
 
 
